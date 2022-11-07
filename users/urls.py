7489ed647fe1d5e6a1b09322f8ec34ea30dcc908
ADESH SUKAYE <adeshsukaye@gmail.com>
2022-11-07 19:14:37 +0530

    first commit of awsome website

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import path,include
+from users.views import dashboard,register
+
+urlpatterns = [
+    path('dashboard/',dashboard,name='dashboard'),
+    path('accounts/',include("django.contrib.auth.urls")),
+    path("register/", register, name="register"),
+    path("oauth/", include("social_django.urls")),
+   
+]
